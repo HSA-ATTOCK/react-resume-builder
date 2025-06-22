31a138110186bc130d47b8524c054460b5c1f951
Haider Sajjad Azmat <haidersajjadazmat@gmail.com>
2025-06-22 12:44:27 +0500

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+const distPath = path.join(__dirname, "../dist");
+app.use(express.static(distPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
+module.exports = app;

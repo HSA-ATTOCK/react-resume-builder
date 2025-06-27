ae10b0cf13cd1768ddf0908f07c0c26bd6fd35cf
HSA-ATTOCK <haidersajjadazmat@gmail.com>
2025-06-27 17:50:39 +0500

    Revert "Merge branch 'main' of https://github.com/HSA-ATTOCK/resume-builder"
    This reverts commit cb1d665afee880f70de5f598830c5b8bb9ff14c7, reversing
    changes made to ce856b53cb4326ec7c4d53c21cee683518044ba6.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require("path");
 const fs = require("fs");
 const express = require("express");
@@ -12,18 +11,3 @@ app.get("*", (req, res) => {
 });
 
 module.exports = app;
-=======
-const path = require("path");
-const fs = require("fs");
-const express = require("express");
-const app = express();
-
-const distPath = path.join(__dirname, "../dist");
-app.use(express.static(distPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(distPath, "index.html"));
-});
-
-module.exports = app;
->>>>>>> da659ab2c95ce83ac9ebcdd9840fce9d04bb7fcb

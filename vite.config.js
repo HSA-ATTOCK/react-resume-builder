72082b89ecc41b4635e9ac3f0f6a86b461491cda
HSA-ATTOCK <haidersajjadazmat@gmail.com>
2025-06-27 17:46:46 +0500

    Revert "errors fixing"
    This reverts commit 3c7edf740c1e88fc69556795315bcdba7cc66ad3.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ Tells Vite to use relative paths (important for Vercel)
   build: {
     outDir: "dist",
   },

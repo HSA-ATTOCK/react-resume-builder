3c7edf740c1e88fc69556795315bcdba7cc66ad3
HSA-ATTOCK <haidersajjadazmat@gmail.com>
2025-06-27 17:38:31 +0500

    errors fixing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ Tells Vite to use relative paths (important for Vercel)
   build: {
     outDir: "dist",
   },
